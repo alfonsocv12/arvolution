5582b9550001ea2a23d269c3eb1f9cd452fd5de1
alfonsocv12 <alfonsocvu@gmail.com>
2022-12-22 20:05:27 -0600

    Add readme change port on env to be on the docker network

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Score Prototype
+
+To start
+
+* Pre-requirements
+    * Docker
+    * docker-compose
+
+Once you have the pre-requirements run
+
+```bash
+$ docker-compose up
+```
+
+* Api will be on port 3000
+* UI will be on port 80
+* Database on port 5432
--- score_api/src/env.ts
+++ score_api/src/env.ts
@@ -1,6 +1,6 @@
 export default new (class Env {
     public docs: boolean = true;
-    public host: string = "192.168.56.100";
+    public host: string = "score_db";
     public port: number = 5432;
     public username: string = "api_user";
     public password: string = "jhpiowuhp10";
